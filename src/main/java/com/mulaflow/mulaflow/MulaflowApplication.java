fb1a351a47339ef83535ae60897fa7c2bb01194c
kinyarasam <skinyara.30@gmail.com>
2025-04-10 20:43:24 +0300

    Initial commit: Spring Boot project setup with dependancies.

---
+++ src/main/java/com/mulaflow/mulaflow/MulaflowApplication.java
@@ -0,0 +1,13 @@
+package com.mulaflow.mulaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulaflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulaflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mulaflow/mulaflow/MulaflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulaflow.mulaflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulaflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
